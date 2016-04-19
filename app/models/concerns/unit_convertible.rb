f22ef5b7e78ead93bb5e1559bf7bf0e3d3b8c5ed
Amin Karaji <amin.karaji@siteminder.com>
2016-04-19 16:13:04 +1000

    exception print with next inside loop + adding a test

--- app/models/concerns/unit_convertible.rb
+++ app/models/concerns/unit_convertible.rb
@@ -7,8 +7,16 @@ module UnitConvertible
 
   module ClassMethods
     def to_milligram
-      self.includes(:unit).where("amount > 0").where(amount_mg: nil).each do |model|
-        model.update_column :amount_mg, model.to_milligram
+      self.joins(:unit).where("amount > 0").where(amount_mg: nil).each do |model|
+        begin
+         model.update_column :amount_mg, model.to_milligram
+        rescue Unitwise::ConversionError => e
+          puts "#{model.id}: #{e.to_s}"
+          next
+        rescue Unitwise::ExpressionError => e
+          puts "#{model.id}: #{e.to_s}"
+          next
+        end
       end
     end
   end
--- spec/concerns/unit_convertible_spec.rb
+++ spec/concerns/unit_convertible_spec.rb
@@ -1,11 +1,38 @@
 require 'spec_helper'
 
 shared_examples "unit_convertible" do
-  let(:model) { described_class }
+  let(:described_symbol) { described_class.to_s.underscore.to_sym }
+  let(:kilojoule) { create :unit, name: 'kilojoule', symbol: 'kj' }
+  let(:invalid_unit) { create :unit, name: 'invalid_unit', symbol: 'w' }
 
-  describe "to_miligram" do
-    it "units are not convertible" do
-      expect(create(model.to_s.underscore.to_sym).to_milligram).to eq(10500.0)
+  describe "#to_milligram" do
+    it "is convertible unit" do
+      expect{ create(described_symbol).to_milligram }.not_to raise_error
+    end
+
+    it "is not convertible unit" do
+      expect{ create(described_symbol, unit: kilojoule).to_milligram }.to raise_error(Unitwise::ConversionError)
+    end
+
+    it "is wrong unit" do
+      expect{ create(described_symbol, unit: invalid_unit).to_milligram }.to raise_error(Unitwise::ExpressionError)
+    end
+  end
+
+  describe ".to_milligram" do
+    before(:each) do
+      @ingredient_nutrition_fact_1 = create described_symbol
+      @ingredient_nutrition_fact_2 = create described_symbol, unit: kilojoule
+      @ingredient_nutrition_fact_3 = create described_symbol, unit: invalid_unit
+      @ingredient_nutrition_fact_4 = create described_symbol
+    end
+
+    it "updates amount_mg for valid ingredient_nutritoin_facts" do
+      described_class.to_milligram
+      expect(@ingredient_nutrition_fact_1.reload.amount_mg).to eq(10500)
+      expect(@ingredient_nutrition_fact_2.reload.amount_mg).to be_nil
+      expect(@ingredient_nutrition_fact_3.reload.amount_mg).to be_nil
+      expect(@ingredient_nutrition_fact_4.reload.amount_mg).to eq(10500)
     end
   end
 end
