cb802b1d6c6584238ef006273b824532c1b1fd8a
Amin Karaji <amin.karaji@siteminder.com>
2016-04-19 12:24:04 +1000

    Fixing a name for a rspec controller test

--- spec/controllers/recipe_categories_controller_spec.rb
+++ spec/controllers/recipe_categories_controller_spec.rb
@@ -18,7 +18,7 @@ require 'rails_helper'
 # Message expectations are only used when there is no simpler way to specify
 # that an instance is receiving a specific message.
 
-RSpec.describe recipeCategoriesController, type: :controller do
+RSpec.describe RecipeCategoriesController, type: :controller do
 
   # This should return the minimal set of attributes required to create a valid
   # recipeCategory. As you add validations to recipeCategory, be sure to
