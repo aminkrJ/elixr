85026b77792f7aeedc7348db250465473979e383
Amin Karaji <amin.karaji@siteminder.com>
2016-04-20 16:33:47 +1000

    Fixing seed for ingredient category

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 if IngredientCategory.count.zero?
-  IngredientCategory.create [{description: "grain (cereal) foods", title: 'ic_1'}, {description: "lean meats and poultry", title: 'ic_2'}, {description: "dairy products", title: 'ic_3'}, {description: "vegtables and beans", title: 'ic_4'}, {description: "vegtables", title: 'ic_5'}, {description: "beans and nuts", title: 'ic_6'}, {description: "fruits", title: 'ic_7'}, {description: "super foods", title: 'ic_8'}]
+  IngredientCategory.create [{description: "grain (cereal) foods", title: 'ic_1'}, {description: "lean meats and poultry", title: 'ic_2'}, {description: "dairy products", title: 'ic_3'}, {description: "vegtables", title: 'ic_4'}, {description: "beans and nuts", title: 'ic_5'}, {description: "fruits", title: 'ic_6'}, {description: "super foods", title: 'ic_7'}]
 end
