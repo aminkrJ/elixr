95d7ec3cb6c61302d384ca2bed716d5018bef1a0
Amin Karaji <amin.karaji@siteminder.com>
2016-05-12 11:02:34 +1000

    Fixing ingredient category view pages

--- app/controllers/admin/ingredient_categories_controller.rb
+++ app/controllers/admin/ingredient_categories_controller.rb
@@ -1,60 +1,48 @@
-class Admin::IngredientCategoriesController < ApplicationController
-  before_action :set_admin_ingredient_category, only: [:show, :edit, :update, :destroy]
+class Admin::IngredientCategoriesController < AdminController
+  before_action :set_ingredient_category, only: [:show, :edit, :update, :destroy]
 
-  # GET /admin/ingredient_categories
-  # GET /admin/ingredient_categories.json
   def index
-    @admin_ingredient_categories = Admin::IngredientCategory.all
+    @ingredient_categories = IngredientCategory.all
   end
 
-  # GET /admin/ingredient_categories/1
-  # GET /admin/ingredient_categories/1.json
   def show
   end
 
-  # GET /admin/ingredient_categories/new
   def new
-    @admin_ingredient_category = Admin::IngredientCategory.new
+    @ingredient_category = IngredientCategory.new
   end
 
-  # GET /admin/ingredient_categories/1/edit
   def edit
   end
 
-  # POST /admin/ingredient_categories
-  # POST /admin/ingredient_categories.json
   def create
-    @admin_ingredient_category = Admin::IngredientCategory.new(admin_ingredient_category_params)
+    @ingredient_category = IngredientCategory.new(ingredient_category_params)
 
     respond_to do |format|
-      if @admin_ingredient_category.save
-        format.html { redirect_to @admin_ingredient_category, notice: 'Ingredient category was successfully created.' }
-        format.json { render :show, status: :created, location: @admin_ingredient_category }
+      if @ingredient_category.save
+        format.html { redirect_to [:admin, @ingredient_category], notice: 'Ingredient category was successfully created.' }
+        format.json { render :show, status: :created, location: @ingredient_category }
       else
         format.html { render :new }
-        format.json { render json: @admin_ingredient_category.errors, status: :unprocessable_entity }
+        format.json { render json: @ingredient_category.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /admin/ingredient_categories/1
-  # PATCH/PUT /admin/ingredient_categories/1.json
   def update
     respond_to do |format|
-      if @admin_ingredient_category.update(admin_ingredient_category_params)
-        format.html { redirect_to @admin_ingredient_category, notice: 'Ingredient category was successfully updated.' }
-        format.json { render :show, status: :ok, location: @admin_ingredient_category }
+      if @ingredient_category.update(ingredient_category_params)
+        format.html { redirect_to [:admin, @ingredient_category], notice: 'Ingredient category was successfully updated.' }
+        format.json { render :show, status: :ok, location: @ingredient_category }
       else
         format.html { render :edit }
-        format.json { render json: @admin_ingredient_category.errors, status: :unprocessable_entity }
+        format.json { render json: @ingredient_category.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /admin/ingredient_categories/1
-  # DELETE /admin/ingredient_categories/1.json
   def destroy
-    @admin_ingredient_category.destroy
+    @ingredient_category.destroy
     respond_to do |format|
       format.html { redirect_to admin_ingredient_categories_url, notice: 'Ingredient category was successfully destroyed.' }
       format.json { head :no_content }
@@ -62,13 +50,11 @@ class Admin::IngredientCategoriesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_admin_ingredient_category
-      @admin_ingredient_category = Admin::IngredientCategory.find(params[:id])
+    def set_ingredient_category
+      @ingredient_category = IngredientCategory.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def admin_ingredient_category_params
-      params.require(:admin_ingredient_category).permit(:title, :color, :description)
+    def ingredient_category_params
+      params.require(:ingredient_category).permit(:title, :color, :description)
     end
 end
--- app/models/admin/ingredient_category.rb
+++
@@ -1,2 +0,0 @@
-class Admin::IngredientCategory < ActiveRecord::Base
-end
