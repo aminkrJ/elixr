e90da516ed1d604f3f0e9cc2e40f722d37b134bd
Amin Karaji <amin.karaji@siteminder.com>
2016-05-16 11:24:10 +1000

    Clean up more unnecessary tests

--- spec/models/admin/ingredient_nutrition_facts_spec.rb
+++ spec/models/admin/ingredient_nutrition_facts_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-require_relative '../concerns/unit_convertible_spec'
+require_relative '../../concerns/unit_convertible_spec'
 
 RSpec.describe IngredientNutritionFact, type: :model do
   it_behaves_like "unit_convertible"
