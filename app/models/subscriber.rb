83dfd0b68e6591a9d8dffc2c34bb64c7b97ce87e
Amin Karaji <amin.karaji@siteminder.com>
2016-09-21 16:39:19 +1000

    Adding email validation for subscribers

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,2 +1,5 @@
 class Subscriber < ActiveRecord::Base
+  validates :email, presence: true
+  validates :email, confirmation: {case_sensitive: false}
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Subscriber, type: :model do
-  it "creates a subscriber" do
-    expect{Subscriber.create(full_name: "Amin Karaji")}.to_not raise_error
+  it "creates a subscriber without email" do
+    expect(build(:subscriber, full_name: "Amin Karaji", email: nil).valid?).to be_falsy
+  end
+
+  it "creates a subscriber with invalid email" do
+    expect(build(:subscriber, full_name: "Amin Karaji", email: "a@b").valid?).to be_falsy
   end
 end
