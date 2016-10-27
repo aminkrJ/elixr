e08a65e391200f695b2f1d105e38252f1e3b4b6e
Amin Karaji <amin.karaji@siteminder.com>
2016-10-27 14:28:55 +1100

    Adding delete for subscribers

--- app/controllers/admin/subscribers_controller.rb
+++ app/controllers/admin/subscribers_controller.rb
@@ -1,5 +1,21 @@
 class Admin::SubscribersController < AdminController
+  before_action :set_subscriber, only: [:destroy]
+
   def index
     @subscribers = Subscriber.all
   end
+
+  def destroy
+    @subscriber.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_subscribers_url, notice: 'Subscriber was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_subscriber
+      @subscriber = Subscriber.find(params[:id])
+    end
 end
