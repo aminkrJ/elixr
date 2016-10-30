04fcba9239bae385df5e3f895a38bf29fe09fe92
Amin Karaji <amin.karaji@siteminder.com>
2016-10-31 09:54:37 +1100

    Adding photo for products

--- app/views/products/index.json.jbuilder
+++ app/views/products/index.json.jbuilder
@@ -1,1 +1,12 @@
-json.array! @products, :id, :name, :description, :price, :primary, :created_at
+json.array! @products do |product|
+ json.id product.id
+ json.name product.name
+ json.description product.description
+ json.price product.price
+ json.primary product.primary
+ json.photo do
+   json.thumb product.photo.url(:thumb)
+   json.medium product.photo.url(:medium)
+ end
+ json.created_at product.created_at
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018003126) do
+ActiveRecord::Schema.define(version: 20161030222627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,10 +112,14 @@ ActiveRecord::Schema.define(version: 20161018003126) do
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
-    t.decimal  "price",       precision: 8, scale: 2
+    t.decimal  "price",              precision: 8, scale: 2
     t.boolean  "primary"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "recipe_categories", force: :cascade do |t|
