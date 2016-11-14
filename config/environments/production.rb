293d06eeae142b294796f3b93d18ca942dd53477
Amin Karaji <amin.karaji@siteminder.com>
2016-11-14 15:55:39 +1100

    removing react configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.react.variant = :production
-
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
