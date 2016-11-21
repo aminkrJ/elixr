d1283c5d58f80c76046db4c217679dc823c28641
Amin Karaji <amin.karaji@siteminder.com>
2016-11-21 14:53:21 +1100

    change the images dimentions

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :photo, styles: { medium: "240x150>", thumb: "160x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 
   scope :primary, -> {where(primary: true)}
