257d60d62811a2383b92b0eb598738449fca3e6c
Amin Karaji <amin.karaji@siteminder.com>
2017-01-01 15:32:26 +1100

    token auth will handle this feature

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -5,12 +5,6 @@ class Api::V1::BaseController < ApplicationController
 
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
-  before_action :destroy_session
-
-  def destroy_session
-    request.session_options[:skip] = true
-  end
-
   def not_found
     return api_error(status: 404, errors: 'Not found')
   end
