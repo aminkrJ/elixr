b79dce6941d10d83df84d0a2377807f8b8d40b54
Amin Karaji <amin.karaji@siteminder.com>
2017-02-23 15:04:53 +1100

    stripe charge foundation

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,4 +1,29 @@
 class Api::V1::OrdersController < Api::V1::BaseController
+
   def create
+    # verifying products
+    # adding products to a cart
+    # create a customer
+    # persist the cart
+    # charge the customer
+    # change the state of the order
+
+
+    @amount = 500 # in cents
+
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => '',
+      :currency    => 'aud'
+    )
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
   end
 end
