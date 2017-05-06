3b95c4aa2a51c4f75c6cdea095965a4dfd4f8247
Amin Karaji <amin.karaji@siteminder.com>
2017-05-06 12:43:14 +1000

    showing carts in admin section

--- app/controllers/admin/carts_controller.rb
+++ app/controllers/admin/carts_controller.rb
@@ -1,5 +1,25 @@
 class Admin::CartsController < AdminController
+  before_action :set_cart, only: [:destroy]
+
   def invoice
     @cart = Cart.find params[:id]
   end
+
+  def index
+    @carts = Cart.includes(:customer).order('created_at DESC').all
+  end
+
+  def destroy
+    @article.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_carts_url, notice: 'Cart was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_cart
+      @article = Cart.find(params[:id])
+    end
 end
