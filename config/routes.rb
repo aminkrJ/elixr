8c9476de2407a774fc1f93bb674834a120eed997
Amin Karaji <amin.krj@gmail.com>
2017-07-12 10:05:45 +1000

    api default format json

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1 do
+    namespace :v1, defaults: {format: 'json'} do
       mount_devise_token_auth_for 'User', at: 'auth'
       resources :ingredients, only: [:index]
       resources :recipe_categories, only: [:index] do
