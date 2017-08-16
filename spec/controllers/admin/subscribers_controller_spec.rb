5b84d3c5532da898c71528d17153cf82ec9ac1d2
Amin Karaji <amin.karaji@siteminder.com>
2017-08-16 14:48:16 +1000

    fixing spec files

--- spec/controllers/admin/subscribers_controller_spec.rb
+++ spec/controllers/admin/subscribers_controller_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describe SubscribersController do
+describe Admin::SubscribersController do
   render_views
   let(:valid_subscriber) { {full_name: "John Foo", email: "john.foo@example.com"} }
 
