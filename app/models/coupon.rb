ec855f4cf695c4e184eb9603dab6601aed06379b
Amin Karaji <amin.karaji@siteminder.com>
2017-08-29 15:04:42 +1000

    changes to coupon to pass tests

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,27 +1,19 @@
 class Coupon < ActiveRecord::Base
   attr_accessor :shipping_fee, :total, :quantity
-
-  def gst
-    0
-  end
-
-  def total
-    subtotal + shipping_fee
-  end
-
-  def subtotal
-    (price + gst) * quantity
-  end
+  attr_reader :discount
 
   def redeemable?
     (rule == 'DFQM2' && quantity > 2) ? true : false
   end
 
   def apply(options)
-    self.shipping_fee, self.price, self.quantity = options[:shipping_fee], options[:price], options[:quantity]
+    self.shipping_fee, self.total, self.quantity = options[:shipping_fee], options[:total], options[:quantity]
     case rule
     when 'DFQM2'
-      self.shipping_fee = 0 if redeemable?
+      if redeemable?
+        @discount = self.shipping_fee
+        self.shipping_fee = 0
+      end
     end
   end
 end
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -4,12 +4,13 @@ RSpec.describe Coupon, type: :model do
   describe "rule1" do
     it "no shipping fee for orders more than two" do
       coupon = create :coupon, rule: 'DFQM2'
-      cart = create :cart, quantity: 3, price: 10, coupon: coupon
+      cart = create :cart, total: 40, coupon: coupon
 
-      coupon.apply({ shipping_fee: Cart::SHIPPING_FEE, quantity: cart.quantity, price: cart.price })
+      coupon.apply({ shipping_fee: 10, quantity: 3, total: 40 })
 
-      expect(cart.total).to eq(30 + Cart::SHIPPING_FEE)
-      expect(cart.coupon.total).to eq(30)
+      expect(cart.coupon.discount).to eq 10
+      expect(cart.coupon.total).to eq 40
+      expect(cart.coupon.shipping_fee).to eq 0
     end
   end
 end
