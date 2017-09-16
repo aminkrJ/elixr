8c2d684d243775cb712c3ebcde832d7a44bbf90e
Amin Karaji <amin.krj@gmail.com>
2017-09-16 13:01:23 +1000

    Using first subdomain for Apartment to work fine in both prd and stg env + exclude www from subdomains

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -4,8 +4,8 @@
 #
 # require 'apartment/elevators/generic'
 # require 'apartment/elevators/domain'
-require 'apartment/elevators/subdomain'
-# require 'apartment/elevators/first_subdomain'
+# require 'apartment/elevators/subdomain'
+require 'apartment/elevators/first_subdomain'
 
 #
 # Apartment Configuration
@@ -93,5 +93,5 @@ end
 # }
 
 # Rails.application.config.middleware.use Apartment::Elevators::Domain
-Rails.application.config.middleware.use Apartment::Elevators::Subdomain
-# Rails.application.config.middleware.use Apartment::Elevators::FirstSubdomain
+# Rails.application.config.middleware.use Apartment::Elevators::Subdomain
+Rails.application.config.middleware.use Apartment::Elevators::FirstSubdomain
