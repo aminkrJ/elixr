51372f0cd621d24042b1ee018536ddbbda78fd61
Amin Karaji <amin.krj@gmail.com>
2017-09-19 19:09:46 +1000

    It is hard to maintain which field should be listed in articles in controller

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::ArticlesController < Api::V1::BaseController
 
   def index
     @articles = if params[:category]
-      Article.published.where(category: params[:category]).select("id, title, description, slug, created_at, category, author").order(created_at: :desc).all
+      Article.published.where(category: params[:category]).order(created_at: :desc).all
     else
-      Article.published.select("id, title, description, slug, created_at, category, author").order(created_at: :desc).all
+      Article.published.order(created_at: :desc).all
     end
   end
 
