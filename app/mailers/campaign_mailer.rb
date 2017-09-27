a7a494f6f454b545cc68fa95d8800cd90493a755
Amin Karaji <amin.karaji@siteminder.com>
2017-09-27 16:12:39 +1000

    Using customised smtp config to send campaign mailer

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -1,4 +1,6 @@
 class CampaignMailer < ApplicationMailer
+  attr_accessor :delivery_options
+
   def send_pdf(campaign, subscriber)
     if campaign.pdf.exists?
       uri = URI.parse campaign.pdf.url
@@ -12,6 +14,20 @@ class CampaignMailer < ApplicationMailer
       to: subscriber.email,
       subject: "#{campaign.pdf_title}",
       from: "#{Tenant.current.app_name} <#{Tenant.current.smtp_username}>",
+      delivery_method_options: delivery_options,
       bcc: "amin.krj@gmail.com")
+  end
+
+  private
+
+  def delivery_options
+    @delivery_options ||= {
+      user_name: Tenant.current.smtp_username,
+      password: Tenant.current.smtp_password,
+      address: "smtp.gmail.com",
+      port: 587,
+      authentication: "plain",
+      enable_starttls_auto: true
+    }
   end
 end
