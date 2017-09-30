e5e133f2929c331570855605c8947a202dbdaff7
Amin Karaji <amin.krj@gmail.com>
2017-09-30 11:33:15 +1000

    making all tests pass

--- spec/models/recipe_ingredient_spec.rb
+++ spec/models/recipe_ingredient_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe RecipeIngredient, type: :model do
     before(:each) do
       @recipe1 = create :recipe
       @recipe2 = create :recipe
-      ingredient1 = create :ingredient
-      ingredient2 = create :ingredient
-      ingredient3 = create :ingredient
+      ingredient1 = create :ingredient, name: "i1"
+      ingredient2 = create :ingredient, name: "i2"
+      ingredient3 = create :ingredient, name: "i3"
 
       create :recipe_ingredient, recipe: @recipe1, ingredient: ingredient1, description: 'ingredient1', amount: 100
       create :recipe_ingredient, recipe: @recipe1, ingredient: ingredient2, description: 'ingredient2', amount: 100
@@ -17,11 +17,11 @@ RSpec.describe RecipeIngredient, type: :model do
 
     it "correct totals for each ingredient" do
       shopping_list = RecipeIngredient.recipes_ingredients([@recipe1.id, @recipe2.id])
-      expect(shopping_list.first.name).to eql('ingredient2')
+      expect(shopping_list.first.name).to eql("i2")
       expect(shopping_list.first.total_amount).to eql(200)
-      expect(shopping_list.second.name).to eql('ingredient1')
+      expect(shopping_list.second.name).to eql("i1")
       expect(shopping_list.second.total_amount).to eql(100)
-      expect(shopping_list.third.name).to eql('ingredient3')
+      expect(shopping_list.third.name).to eql("i3")
       expect(shopping_list.third.total_amount).to eql(50)
     end
   end
