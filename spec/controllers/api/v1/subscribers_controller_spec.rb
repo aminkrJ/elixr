059ca7629141ab3ea7ad259c16b1abe6430789ce
Amin Karaji <amin.krj@gmail.com>
2017-09-30 11:22:31 +1000

    rspec controller tests for nested resources

--- spec/controllers/api/v1/subscribers_controller_spec.rb
+++ spec/controllers/api/v1/subscribers_controller_spec.rb
@@ -4,11 +4,11 @@ describe Api::V1::SubscribersController do
   render_views
 
   let(:valid_subscriber) { {full_name: "John Foo", email: "john.foo@example.com" } }
-
+  let(:campaign) { create :campaign }
 
   it "create a new subscriber" do
     expect {
-      post :create, { subscriber: valid_subscriber }
+      post :create, campaign_id: campaign.id, subscriber: valid_subscriber
     }.to change(Subscriber, :count).by(1)
   end
 
