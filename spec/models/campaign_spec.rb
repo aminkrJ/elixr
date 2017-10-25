b09e0aa24c3160ed5e20c5465250e770c778ddd8
Amin Karaji <amin.krj@gmail.com>
2017-10-25 12:09:39 +1100

    check counter cache for campaign works

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Campaign, type: :model do
-  describe "#launch" do
+  describe "has subscriber's count" do
+    it "inc subscribers" do
+      campaign = create :campaign
+      campaign.subscribers << create(:subscriber)
+
+      expect(campaign.campaign_subscribers_count).to eq(1)
+    end
   end
 end
