ce5a1ae247bf235adf641067d7cbe70b52f5e409
Amin Karaji <amin.krj@gmail.com>
2017-10-26 11:39:43 +1100

    email address must be unique

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -2,6 +2,7 @@ class Subscriber < ActiveRecord::Base
   validates :email, presence: true
   validates :email, confirmation: { case_sensitive: false }
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates_uniqueness_of :email, message: "already exists."
 
   has_many :campaign_subscribers
   has_many :campaigns, through: :campaign_subscribers
