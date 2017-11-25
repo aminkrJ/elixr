7fc79853990bef19e609a54902fd10aa75de4f81
Amin Karaji <amin.krj@gmail.com>
2017-11-25 14:16:05 +1100

    change accepted postcodes for delivery

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,9 +1,14 @@
 class Address < ActiveRecord::Base
+  ACCEPTED_POSTCODES = ["2072", "2042"]
+
   belongs_to :address
 
   validates :street_address, :suburb, :city, :state, :zip, presence: true
+  validates :zip, inclusion: { in: ACCEPTED_POSTCODES,
+                                message: "We only deliver to #{ACCEPTED_POSTCODES.map(&:inspect).join(', ')} postcodes" }
 
   before_save :australia_wide_delivery
+
 
   private
 
