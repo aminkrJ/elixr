40c2b6ee975d1b49f558c1e40ecc397bd9bc2f8a
Amin Karaji <amin.krj@gmail.com>
2017-12-02 16:13:58 +1100

    order by updated at to use it for sorting products in frontend

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,15 +1,15 @@
 class Api::V1::ProductsController < Api::V1::BaseController
   def index
-    @products = Product.includes(:ingredients, :product_photos, :recipes).order(created_at: :desc).all
+    @products = Product.includes(:ingredients, :product_photos, :recipes).order(updated_at: :desc).all
   end
 
   def menu
-    @products = Product.includes(:ingredients, :product_photos, :recipes).where(primary: false).order(created_at: :desc).all
+    @products = Product.includes(:ingredients, :product_photos, :recipes).where(primary: false).order(updated_at: :desc).all
     render :index
   end
 
   def bundles
-    @products = Product.includes(:ingredients, :product_photos, :recipes).where(primary: true).order(created_at: :desc).all
+    @products = Product.includes(:ingredients, :product_photos, :recipes).where(primary: true).order(updated_at: :desc).all
     render :index
   end
 
