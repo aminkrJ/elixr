87c2f606b683bc69107896a503bf599f8dc983eb
Amin Karaji <amin.krj@gmail.com>
2017-12-13 11:44:02 +1100

    remove unnec

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,16 +3,6 @@ class Api::V1::ProductsController < Api::V1::BaseController
     @products = Product.includes(:ingredients, :product_photos, :recipes).order(updated_at: :desc).all
   end
 
-  def menu
-    @products = Product.includes(:ingredients, :product_photos, :recipes).where(primary: false).order(updated_at: :desc).all
-    render :index
-  end
-
-  def bundles
-    @products = Product.includes(:ingredients, :product_photos, :recipes).where(primary: true).order(updated_at: :desc).all
-    render :index
-  end
-
   def show
     @product = Product.includes(:recipes).friendly.find(params[:id])
   end
