a9fe42d9c4c35a91520968fb34d5178306262c30
Amin Karaji <amin.krj@gmail.com>
2018-01-06 09:43:10 +1100

    Destroy postcode

--- app/controllers/admin/postcodes_controller.rb
+++ app/controllers/admin/postcodes_controller.rb
@@ -1,5 +1,23 @@
 class Admin::PostcodesController < AdminController
+  before_action :set_postcode, only: [:show, :edit, :update, :destroy]
+
   def index
     @postcodes = Postcode.order("count DESC").all
   end
+
+  def destroy
+    @postcode.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_postcodes_url, notice: 'Postcode was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_postcode
+      @postcode = Postcode.find(params[:id])
+    end
+
+
 end
